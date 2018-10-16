ddbd89cb240ec0e8abc8a96593cc9b51149c7c6b
Zanidd <ninijay93@gmail.com>
2018-10-16 10:35:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const
-  http = require('http'),
-  fs = require('fs');
-  url = require('url'),
-  path = require('path');
-
-server = http.createServer(function(request, response) {
-    var uri = url.parse(request.url).pathname,
-        filename = path.join(process.cwd(), uri);
-    console.log("Serving: " + filename);
-    fs.readFile(filename, 'binary', function(err, file) {
-        response.writeHead(200);
-        response.write(file, 'binary');
-        response.end();
-    })
-});
-
-server.listen(8080, function(){
-    console.log('listening on 8080');
+ http = require('http'),
+ fs = require('fs');
+ url = require('url'),
+ path = require('path');server = http.createServer(function(request, response) {
+   var uri = url.parse(request.url).pathname,
+       filename = path.join(process.cwd(), uri);
+   console.log("Serving: " + filename);
+   fs.readFile(filename, 'binary', function(err, file) {
+        if(err){
+            response.writeHead(404);
+            response.write("Not Found");
+            response.end();
+        }
+        else{
+            response.writeHead(200);
+            response.write(file, 'binary');
+            response.end();
+        }
+   })
+});server.listen(8080, function(){
+   console.log('listening on 8080');
 });
